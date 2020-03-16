fc770652ec221298d8d44775559333c5f8265fad
Kacper Latuszewski <kacpx123@gmail.com>
2020-03-16 07:39:18 +0100

    working changes

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
-import numpy as np
-import cv2
+import cv2;
+import numpy as np;
 
 cap = cv2.VideoCapture('starlink.mp4')
-lower = np.array([0, 0, 100])
-upper = np.array([10, 30, 100])
-kernelOpen = np.ones((5, 5))
 
 while(1):
     _, frame = cap.read()
 
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    template  = cv2.imread('template.jpg', 0);
 
-    mask = cv2.inRange(hsv_frame, lower, upper)
-    result = cv2.bitwise_and(hsv_frame, hsv_frame, mask=mask)
+    w,h = template.shape[::-1]
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    start_point = (50, 100) 
-    
-    end_point = (100, 50) 
-    
-    color = (0, 0, 255) 
-    
-    thickness = 1
+    result = cv2.matchTemplate(gray_image, template, cv2.TM_CCOEFF_NORMED)
+    threh = 0.4
+    loc = np.where(result>=threh)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(frame, pt, (pt[0]+w,pt[1]+h),(0,255,255),1)
 
-    #frame = cv2.rectangle(frame, start_point, end_point, color, thickness) 
+    cv2.imshow('starlink', frame);
 
-    cv2.imshow('frame', frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('result', result)
+    _, threshold =  cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY, dst=None);
+    _, threshold2 =  cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY, dst=None);
+
 
     if cv2.waitKey(60) & 0xFF == ord('q'):
         break
 
-cv2.destroyAllWindows()
-cap.release()
+
+
+cv2.waitKey(0);
+cv2.destroyAllWindows();
